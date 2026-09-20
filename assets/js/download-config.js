8cf5b37c39e7830a897d01e4b8eef0a82e85b2bc
Eddyo23 <oweneddy4@gmail.com>
2026-09-20 16:04:32 -0500

    Enable verified Stripe contribution emails

--- assets/js/download-config.js
+++ assets/js/download-config.js
@@ -1,2 +1,2 @@
-// Enable after Stripe webhook and email delivery are verified.
-window.SCORESTEMS_DOWNLOADS = { stripePaymentLink: 'https://buy.stripe.com/00w8wR3WK63M20ueKR67S00', contributionsEnabled: false };
+// Live Stripe webhook and email delivery verified.
+window.SCORESTEMS_DOWNLOADS = { stripePaymentLink: 'https://buy.stripe.com/00w8wR3WK63M20ueKR67S00', contributionsEnabled: true };
